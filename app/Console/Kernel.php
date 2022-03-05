b843d745092c655e247a9cd67323a2bd79e66e64
Robin Martijn <ik@robinmartijn.nl>
2022-03-05 14:42:23 +0100

    ✨ schedule the caching

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // Schedule workspaces:cache to run every day at midnight
+        $schedule->command('workspaces:cache')->dailyAt('00:00');
     }
 
     /**
